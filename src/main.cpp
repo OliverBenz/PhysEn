3f5bdc3d615b7f5779f0c8204b66bb0d1a12cf6f
OliverBenz <oliver.benz@outlook.com>
2020-12-03 19:38:23 +0100

    Added g-unit tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include <gtest/gtest.h>
+
 #include <iostream>
 #include "maths/Vector.hpp"
 #include "maths/Matrix.hpp"
@@ -7,7 +9,10 @@
 
 using namespace Physics;
 
-bool test_update(){
+// -------------------
+// ----- Physics -----
+// -------------------
+TEST(Physics, UpdateTest){
     Objects::Object obj(
         Maths::Vector(0.0f, 100.0f, 0.0f),
         Maths::Vector(20.0f, 0.0f, 0.0f),
@@ -22,26 +27,38 @@ bool test_update(){
 
     obj1.update((float)t);
 
-    return obj.getPosition() == obj1.getPosition() && obj.getVelocity() == obj1.getVelocity() && obj.getAcceleration() == obj1.getAcceleration();
+    EXPECT_EQ(obj.getPosition(), obj1.getPosition());
+    EXPECT_EQ(obj.getVelocity(), obj1.getVelocity());
+    EXPECT_EQ(obj.getAcceleration(), obj1.getAcceleration());
 }
 
-bool test_cross_product(){
+// -------------------
+// ------ Maths ------
+// -------------------
+TEST(Maths, VectorCrossProduct){
     Maths::Vector a(2, 3, 4);
     Maths::Vector b(5, 6, 7);
     Maths::Vector c = a.getCrossProduct(b);
 
-    return c.x == -3 && c.y == 6 && c.z == -3;
+    ASSERT_EQ(c.x, -3);
+    ASSERT_EQ(c.y, 6);
+    ASSERT_EQ(c.z, -3);
 }
 
-bool test_vec_operators(){
+TEST(Maths, VectorOperations){
     Maths::Vector a(1, 1, 2);
     Maths::Vector b(1, 1, 2);
 
-    // +, -, *, /, ==, !=
-
-    return a == b && !(a != b) && ((a + b) == Maths::Vector(2, 2, 4)) && (a*b == Maths::Vector(1, 1, 4));
+    // ==
+    EXPECT_EQ(a, b);
+    // !=
+    EXPECT_EQ(!(a != b), true);
+    // +
+    EXPECT_EQ(a+b, Maths::Vector(2, 2, 4));
+    // *
+    EXPECT_EQ(a*b, Maths::Vector(1, 1, 4));
 }
-
+/*
 bool test_matrix(){
     // TODO: Fix operators, research
     Maths::Matrix m1(Maths::Unity, Size(5, 5));
@@ -63,25 +80,10 @@ bool test_matrix(){
 
     return false;
 }
+*/
 
-int main(){
-	std::cout << "Library version: " << VERSION_MAJOR << "." << VERSION_MINOR << "." << VERSION_PATCH << "\n";
-    // Vector tests
-    std::cout << "Vector Test results:" << "\n";
-    std::cout << "Operators:\t" << (test_vec_operators() ? "True" : "False") << "\n";
-    std::cout << "Cross-Product:\t" << (test_cross_product() ? "True" : "False") << "\n";
+int main(int argc, char **argv){
+	testing::InitGoogleTest(&argc, argv);
 
-    std::cout << "\n";
-    std::cout << "Object Update:\t" << (test_update() ? "True" : "False") << "\n";
-
-    Objects::CircleObject circ(
-        Maths::Vector(1, 2, 0),
-        5800
-    );
-
-    std::cout << circ.getCentripedalAcc(694) << "\n";
-
-    if(test_matrix()){}
-
-    return 0;
+    return RUN_ALL_TESTS();
 }
