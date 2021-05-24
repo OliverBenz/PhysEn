d5956b664fb493b1934774343c2b7fb5ae9f2743
OliverBenz <oliver.benz@outlook.com>
2021-05-24 18:51:49 +0200

    General: Add constants header

---
+++ src/Constants.hpp
@@ -0,0 +1,18 @@
+namespace PhysEn{
+
+// ------- Mathematical -------
+// Pi
+constexpr double PI = 3.14159265358979323846;
+
+
+// ------- Physical -------
+// Electric Field Constant [As/Vm]
+constexpr double ELECTRIC_MU = 8.8541878128e-12;
+
+// Magnetic Field Constant [N/A^2]
+constexpr double MAGNETIC_MU = 1.25663706212e-6;
+
+// Gravitational Acceleration [m/s^2]
+constexpr double GRAVITY_ACC = 9.80665;
+
+}
