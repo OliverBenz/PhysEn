cd75984d8a793d5725f558e13aaeb7eda354987a
OliverBenz <oliver.benz@outlook.com>
2021-06-11 20:29:50 +0200

    Maths: Add complex number division

--- src/maths/Complex.cpp
+++ src/maths/Complex.cpp
@@ -51,6 +51,64 @@ Complex &operator*=(Complex &left, Complex &right) {
 	return left;
 }
 
+// Division
+Complex operator/(float left, Complex& right){
+	if(right == 0)
+		throw std::invalid_argument("Cannot divide by zero!");
+
+	// float left === Complex(left, 0)
+	return {
+		(left*right[0]) / (right[0]*right[0] + right[1]*right[1]),
+		(-left*right[1]) / (right[0]*right[0] + right[1]*right[1]),
+	};
+}
+Complex operator/(Complex& left, float right){
+	if (right == 0)
+		throw std::invalid_argument("Cannot divide by zero!");
+
+	// float right === Complex(right, 0)
+	return {
+		left[0] / right,
+		left[1]/ right
+	};
+}
+Complex operator/(Complex& left, Complex& right){
+	if(right == 0)
+		throw std::invalid_argument("Cannot divide by zero!");
+
+	return {
+		(left[0]*right[0]+left[1]*right[1]) / (right[0]*right[0] + right[1]*right[1]),
+		(left[1]*right[0]-left[0]*right[1]) / (right[0]*right[0] + right[1]*right[1])
+	};
+}
+Complex& operator/=(Complex& left, const float right){
+	if (right == 0)
+		throw std::invalid_argument("Cannot divide by zero!");
+
+	// float right === Complex(right, 0)
+	Complex result{
+			left[0] / right,
+			left[1]/ right
+	};
+
+	left[0] = result[0];
+	left[1] = result[1];
+	return left;
+}
+Complex& operator/=(Complex& left, Complex& right){
+	if(right == 0)
+		throw std::invalid_argument("Cannot divide by zero!");
+
+	Complex result{
+		(left[0]*right[0]+left[1]*right[1]) / (right[0]*right[0] + right[1]*right[1]),
+		(left[1]*right[0]-left[0]*right[1]) / (right[0]*right[0] + right[1]*right[1])
+	};
+
+	left[0] = result[0];
+	left[1] = result[1];
+	return left;
+}
+
 // Addition
 Complex operator+(Complex& left, Complex& right){
 	return {left[0]+right[0], left[1]+right[1]};
@@ -98,6 +156,12 @@ Complex& operator-=(Complex& left, float right){
 // Equality
 bool operator==(Complex &left, Complex &right) {
 	return left[0] == right[0] && left[1] == right[1];
+}
+bool operator==(Complex& left, float right){
+	return left[0] == right && left[1] == 0;
+}
+bool operator==(float left, Complex& right){
+	return right[0] == left && right[1] == 0;
 }
 
 // Extra
