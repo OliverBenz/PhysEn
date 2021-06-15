e2a0b3dcbb42c7513bd49628e5fae5fd7906c977
OliverBenz <oliver.benz@outlook.com>
2021-06-15 12:23:12 +0200

    Optics: Add lens and path matrix generator function.

---
+++ tests/Physics/Optics.GTest.cpp
@@ -0,0 +1,22 @@
+#include "gtest/gtest.h"
+
+#include "Physics/Optics.hpp"
+#include "Maths/Vector.hpp"
+
+namespace PhysEn {
+namespace GTest {
+
+TEST(Optics, Path) {
+    Maths::Vector light{2, 0.2};
+
+    Optics::Lens lens;
+    lens.n = 1.5;
+    lens.r1 = 0.02;
+    lens.r2 = -0.04;
+    lens.w = 0.03;
+
+    Maths::Matrix lensMatrix = Optics::GetLens(lens, 1, 1);
+}
+
+}
+}
