bc019dc009e6ad65b862037ef32ebaf62672afa6
Oliver Benz <oliver.benz@outlook.com>
2022-04-21 21:26:59 +0200

    CMake: Properly setup gtest project.

---
+++ tests/maths/Files.cmake
@@ -0,0 +1,6 @@
+set(mathTests
+    ${CMAKE_CURRENT_SOURCE_DIR}/maths/complex.gtest.cpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/maths/integration.gtest.cpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/maths/matrix.gtest.cpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/maths/vector.gtest.cpp
+)
