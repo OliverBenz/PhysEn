ddc5c907f32ce054b1707100c5d63dec53ab2a7b
Oliver Benz <oliver.benz@outlook.com>
2022-04-21 21:05:16 +0200

    CMake: Copy header files to output.

---
+++ src/maths/Files.cmake
@@ -0,0 +1,12 @@
+set(mathHeaders
+    ${CMAKE_CURRENT_SOURCE_DIR}/maths/complex.hpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/maths/integration.hpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/maths/matrix.hpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/maths/matrixSolver.hpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/maths/vector.hpp
+)
+
+set(mathSources
+    ${CMAKE_CURRENT_SOURCE_DIR}/maths/complex.cpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/maths/integration.cpp
+)
---
+++ src/physics/Files.cmake
@@ -0,0 +1,15 @@
+set(physHeaders
+    ${CMAKE_CURRENT_SOURCE_DIR}/physics/object.hpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/physics/circle.hpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/physics/rectangle.hpp
+
+    ${CMAKE_CURRENT_SOURCE_DIR}/physics/optics.hpp
+)
+
+set(physSources
+    ${CMAKE_CURRENT_SOURCE_DIR}/physics/object.cpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/physics/circle.cpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/physics/rectangle.cpp
+
+    ${CMAKE_CURRENT_SOURCE_DIR}/physics/optics.cpp
+)
