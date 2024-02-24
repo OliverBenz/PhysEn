d9d393c918d43348ccc804f36f6b3e8d86cd47f4
Oliver Benz <oliver.benz@outlook.com>
2024-02-24 11:01:37 +0100

    CMake: Use C++20 and remove fmt lib.

--- lib/FetchLibraries.cmake
+++ lib/FetchLibraries.cmake
@@ -2,17 +2,6 @@ cmake_minimum_required(VERSION 3.14)
 
 include(FetchContent)
 
-# fmt
-# Version 9.1.0
-FetchContent_Declare(fmt
-  GIT_REPOSITORY https://github.com/fmtlib/fmt.git
-  GIT_TAG        a33701196adfad74917046096bf5a2aa0ab0bb50
-)
-
-FetchContent_MakeAvailable(fmt)
-set_target_properties(fmt PROPERTIES FOLDER "${ideFolderExternal}")
-
-
 # GTest
 # Version 1.13.0
 FetchContent_Declare(
